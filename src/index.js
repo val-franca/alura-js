455f81ed66e9e77e31dc7dc8151a41c092062660
val-franca <valdilenefrancaduda@gmail.com>
2023-03-03 12:44:44 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import fs from 'fs';
+import chalk from 'chalk';
+
+function extraiLinks(texto){
+const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+const capturas = [... texto.matchAll(regex)];
+const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+ return resultados.length !== 0 ? resultados: 'nao ha links no arquivo';
+}
+
+function trataErro(erro) {
+    console.log(erro);
+    throw new Error(chalk.red(erro.code, ' nao ha arquivo no diretorio'));
+}
+//async/await
+
+  async function pegaArquivo(caminhoDoArquivo){
+  try {
+      const encoding = 'utf-8';
+      const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+     return extraiLinks(texto);
+   }  catch (erro) {
+      trataErro(erro)
+   }
+}
+
+pegaArquivo('./arquivos/texto.md');
+
+export default pegaArquivo;
+//\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
